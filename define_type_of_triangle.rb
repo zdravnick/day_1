daa126fe4b30bf3d766700687842c78099389514
zdravnick <zdravnick@gmail.com>
2020-08-27 10:57:37 +0500

    First lesson answers. Corrected

--- define_type_of_triangle.rb
+++ define_type_of_triangle.rb
@@ -1,26 +1,36 @@
 
 # прямоугольный треугольник
-def define_type_of_triangle
-  triangle = []
-  puts 'Введи длину первой стороны треугольника(сантиметры)'
-  puts "Принято: первая сторона равна #{first_side = gets.chomp.to_f} см"
-  puts 'Введи длину второй стороны треугольника(сантиметры)'
-  puts "Принято: вторая сторона равна #{second_side = gets.chomp.to_f} см"
-  puts 'Введи длину третьей стороны треугольника(сантиметры)'
-  puts "Принято: третья сторона равна #{third_side = gets.chomp.to_f} см"
-  triangle  << first_side << second_side << third_side
-  puts "triangle = #{triangle.sort!.reverse!}" # проверяем заполнение массива
-  longest_side = triangle.first
-  middle_side = triangle.second
-  shortest_side = triangle.third
-  if longest_side**2  == middle_side**2  + shortest_side**2
-    puts 'Треугольник прямоугольный'
-  elsif longest_side == middle_side && longest_side == shortest_side && middle_side == shortest_side
-    puts 'Треугольник равносторонний'
-  elsif longest_side == middle_side || longest_side == shortest_side || middle_side == shortest_side
-    puts 'Треугольник равнобедренный'
-  else
-    puts 'Треугольник самый обычный'
-  end
+# задаем массив для сторон треугольника
+triangle = []
+puts 'Введи длину первой стороны треугольника(сантиметры)'
+puts "Принято: первая сторона равна #{first_side = gets.chomp.to_f} см"
+
+puts 'Введи длину второй стороны треугольника(сантиметры)'
+puts "Принято: вторая сторона равна #{second_side = gets.chomp.to_f} см"
+
+puts 'Введи длину третьей стороны треугольника(сантиметры)'
+puts "Принято: третья сторона равна #{third_side = gets.chomp.to_f} см"
+
+triangle  << first_side << second_side << third_side
+
+# проверяем заполнение массива и сортируем по убыванию
+puts "triangle = #{triangle.sort!.reverse!}"
+longest_side = triangle.first
+middle_side = triangle.second
+shortest_side = triangle.third
+
+# определяем тип треугольника
+if longest_side**2  == middle_side**2  + shortest_side**2
+  puts 'Треугольник прямоугольный'
+elsif triangle.uniq.count == 1
+  puts 'Треугольник равносторонний'
+elsif triangle.uniq.count == 2
+  puts 'Треугольник равнобедренный'
+else
+  puts 'Треугольник самый обычный'
 end
-define_type_of_triangle
+
+=begin
+first_side, second_side, third_side = triangle
+у меня отказывается работать наотрез
+=end
